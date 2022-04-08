89b30584e285ee47b1b7f629aa19b54a8bc5d3f3
andrey-rsn <korowai98.ag@gmail.com>
2022-04-08 21:45:17 +0300

    FixFileConverter

--- WeatherArchive.Tests/ExcelFileConverterTests.cs
+++ WeatherArchive.Tests/ExcelFileConverterTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
 using WeatherArchive.Models.DTOs;
 using WeatherArchive.Services.FileConverter;
 
@@ -19,7 +20,7 @@ namespace WeatherArchive.Tests
 
         private string GetTestPath(string fileName)
         {
-            return Path.Combine(_projectDir,"TestFIles",fileName);
+            return Path.Combine(_projectDir, "TestFIles", fileName);
         }
 
         [SetUp]
@@ -33,68 +34,73 @@ namespace WeatherArchive.Tests
         public void ConvertFile_CorrectFile_CorrectData()
         {
             List<WeatherConditionsDTO> resultList = new List<WeatherConditionsDTO>();
-            List<WeatherConditionsDTO> expectedResult=new();
+            List<WeatherConditionsDTO> expectedResult = TestCase1;
             using (FileStream file = new FileStream(GetTestPath("Test1.xlsx"), FileMode.Open, FileAccess.Read))
             {
-               resultList.AddRange(_fileConverter.ConvertFile(file));
+                resultList.AddRange(_fileConverter.ConvertFile(file));
             }
-            Assert.IsTrue(expectedResult.Equals(resultList));
+            var result= JsonConvert.SerializeObject(resultList);
+            var expected = JsonConvert.SerializeObject(expectedResult);
+            Assert.AreEqual(expected, result);
         }
 
-
-        /*private static List<WeatherConditionsDTO> TestCase1 = new List<WeatherConditionsDTO>()
+        private static List<WeatherConditionsDTO> TestCase1 = new List<WeatherConditionsDTO>()
         {
-            new WeatherConditionsDTO()
-            {
-
-                Date = DateTime.Parse("01.01.2010"),
-                Time = DateTime.Parse("00:00"),
-                AirTemerature = -5.5,
-                RelativeHumidity = 89,
-                Td = -6.9,
-                AtmosphericPressure = 737,
-                WindDirection = "З,ЮЗ",
-                WindSpeed = 1,
-                CloudCover = 100,
-                H = 800,
-                VV = null,
-                WeatherPhenomena = "Дымка"
-            },
-            new WeatherConditionsDTO()
-            {
-
-                Date = DateTime.Parse("01.01.2010"),
-                Time = DateTime.Parse("03:00"),
-                AirTemerature = -6,
-                RelativeHumidity = 91,
-                Td = -7.1,
-                AtmosphericPressure = 738,
-                WindDirection = "штиль",
-                WindSpeed = 0,
-                CloudCover = 100,
-                H = 800,
-                VV = null,
-                WeatherPhenomena = "Дымка"
-            },
-            new WeatherConditionsDTO()
-            {
-
-                Date = DateTime.Parse("01.01.2010"),
-                Time = DateTime.Parse("09:00"),
-                AirTemerature = -11.5,
-                RelativeHumidity = 89,
-                Td = -12.8,
-                AtmosphericPressure = 739,
-                WindDirection = "штиль",
-                WindSpeed = 0,
-                CloudCover = 90,
-                H = 800,
-                VV = 4,
-                WeatherPhenomena = "Дымка"
-            }
 
 
-        };*/
 
-    }
+                    new WeatherConditionsDTO()
+                    {
+
+                        Date = DateTime.Parse("01.01.2010"),
+                        Time = TimeSpan.Parse("00:00"),
+                        AirTemerature = -5.5,
+                        RelativeHumidity = 89,
+                        Td = -6.9,
+                        AtmosphericPressure = 737,
+                        WindDirection = "З,ЮЗ",
+                        WindSpeed = 1,
+                        CloudCover = 100,
+                        H = 800,
+                        VV = null,
+                        WeatherPhenomena = "Дымка"
+                    },
+                    new WeatherConditionsDTO()
+                    {
+
+                        Date = DateTime.Parse("01.01.2010"),
+                        Time = TimeSpan.Parse("03:00"),
+                        AirTemerature = -6,
+                        RelativeHumidity = 91,
+                        Td = -7.1,
+                        AtmosphericPressure = 738,
+                        WindDirection = "штиль",
+                        WindSpeed = 0,
+                        CloudCover = 100,
+                        H = 800,
+                        VV = null,
+                        WeatherPhenomena = "Дымка"
+                    },
+                    new WeatherConditionsDTO()
+                    {
+
+                        Date = DateTime.Parse("01.01.2010"),
+                        Time = TimeSpan.Parse("09:00"),
+                        AirTemerature = -11.5,
+                        RelativeHumidity = 89,
+                        Td = -12.8,
+                        AtmosphericPressure = 739,
+                        WindDirection = "штиль",
+                        WindSpeed = 0,
+                        CloudCover = 90,
+                        H = 800,
+                        VV = 4,
+                        WeatherPhenomena = "Дымка"
+                    }
+
+
+        };
+
+
+    } 
 }
--- WeatherArchive/Services/FileConverter/ExcelFileConverter.cs
+++ WeatherArchive/Services/FileConverter/ExcelFileConverter.cs
@@ -31,8 +31,12 @@ namespace WeatherArchive.Services.FileConverter
                 }
 
                 List<WeatherConditionsDTO> weatherConditionsList = new List<WeatherConditionsDTO>();
-
-                for (int i = 0; i < 12; i++)
+                var sheetsNumber = workbook.NumberOfSheets;
+                if(sheetsNumber==0)
+                {
+                    throw new Exception();
+                }
+                for (int i = 0; i < sheetsNumber; i++)
                 {
                     ISheet sheet = workbook.GetSheetAt(i);
 
@@ -46,28 +50,68 @@ namespace WeatherArchive.Services.FileConverter
                     {
                         throw new Exception();
                     }
-                    for(int j = 4; j < rowCount; j++)
+                    for(int j = 4; j < rowCount+1; j++)
                     {
                         var Row=sheet.GetRow(j);
                         if (Row == null)
                         {
                             throw new Exception();
                         }
-                        
+                        DataFormatter formatter = new DataFormatter();
+
+                        //Row.GetCell(10).CellType
+                        //int VVParse;
+                        //int? vv;
+                        //var result=Int32.TryParse(Row.GetCell(10).StringCellValue.Trim(),out VVParse);
+                        //if(!result)
+                        //{
+                        //    vv=null;
+                        //}
+                        //else
+                        //{
+                        //    vv = VVParse;
+                        //}
+                        //WeatherConditionsDTO weatherConditionsFromFile = new WeatherConditionsDTO()
+                        //{
+                        //    Date = Row.GetCell(0).DateCellValue.Date,
+                        //    Time = Row.GetCell(1).DateCellValue.TimeOfDay,
+                        //    AirTemerature = (double)Row.GetCell(2).NumericCellValue,
+                        //    RelativeHumidity = (int)Row.GetCell(3).NumericCellValue,
+                        //    Td = (double)Row.GetCell(4).NumericCellValue,
+                        //    AtmosphericPressure = (int)Row.GetCell(5).NumericCellValue,
+                        //    WindDirection = Row.GetCell(6).StringCellValue.Trim(),
+                        //    WindSpeed = (int)Row.GetCell(7).NumericCellValue,
+                        //    CloudCover = (int)Row.GetCell(8).NumericCellValue,
+                        //    H = (int)Row.GetCell(9).NumericCellValue,
+                        //    VV = vv,
+                        //    WeatherPhenomena = Row.GetCell(11).StringCellValue.Trim()
+                        //};
+                        //var Date = DateTime.Parse(formatter.FormatCellValue(Row.GetCell(0)).Trim());
+                        //var Time = TimeSpan.Parse(formatter.FormatCellValue(Row.GetCell(1)).Trim());
+                        //var AirTemerature = double.Parse(formatter.FormatCellValue(Row.GetCell(2)).Trim());
+                        //var RelativeHumidity = Int32.Parse(formatter.FormatCellValue(Row.GetCell(3)).Trim());
+                        //var Td = double.Parse(formatter.FormatCellValue(Row.GetCell(4)).Trim());
+                        //var AtmosphericPressure = Int32.Parse(formatter.FormatCellValue(Row.GetCell(5)).Trim());
+                        //var WindDirection = formatter.FormatCellValue(Row.GetCell(6)).Trim();
+                        //var WindSpeed = Int32.Parse(formatter.FormatCellValue(Row.GetCell(7)).Trim());
+                        //var CloudCover = Int32.Parse(formatter.FormatCellValue(Row.GetCell(8)).Trim());
+                        //var H = Int32.Parse(formatter.FormatCellValue(Row.GetCell(9)).Trim());
+                        //var VV = GetValueOrNull<int>(formatter.FormatCellValue(Row.GetCell(10)).Trim());
+                        //var WeatherPhenomena = formatter.FormatCellValue(Row.GetCell(11)).Trim();
                         WeatherConditionsDTO weatherConditionsFromFile = new WeatherConditionsDTO()
                         {
-                            Date = Row.GetCell(0).DateCellValue.Date,
-                            Time = Row.GetCell(1).DateCellValue.TimeOfDay,
-                            AirTemerature = Double.Parse(Row.GetCell(2).StringCellValue.Trim()),
-                            RelativeHumidity = Int32.Parse(Row.GetCell(3).StringCellValue.Trim()),
-                            Td = Double.Parse(Row.GetCell(4).StringCellValue.Trim()),
-                            AtmosphericPressure = Int32.Parse(Row.GetCell(5).StringCellValue.Trim()),
-                            WindDirection = Row.GetCell(6).StringCellValue.Trim(),
-                            WindSpeed = Int32.Parse(Row.GetCell(7).StringCellValue.Trim()),
-                            CloudCover = Int32.Parse(Row.GetCell(8).StringCellValue.Trim()),
-                            H = Int32.Parse(Row.GetCell(9).StringCellValue.Trim()),
-                            VV = Int32.Parse(Row.GetCell(10).StringCellValue.Trim()),
-                            WeatherPhenomena = Row.GetCell(11).StringCellValue.Trim()
+                            Date = DateTime.Parse(formatter.FormatCellValue(Row.GetCell(0)).Trim()),
+                            Time = TimeSpan.Parse(formatter.FormatCellValue(Row.GetCell(1)).Trim()),
+                            AirTemerature = double.Parse(formatter.FormatCellValue(Row.GetCell(2)).Trim()),
+                            RelativeHumidity = Int32.Parse(formatter.FormatCellValue(Row.GetCell(3)).Trim()),
+                            Td = double.Parse(formatter.FormatCellValue(Row.GetCell(4)).Trim()),
+                            AtmosphericPressure = Int32.Parse(formatter.FormatCellValue(Row.GetCell(5)).Trim()),
+                            WindDirection = formatter.FormatCellValue(Row.GetCell(6)).Trim(),
+                            WindSpeed = Int32.Parse(formatter.FormatCellValue(Row.GetCell(7)).Trim()),
+                            CloudCover = Int32.Parse(formatter.FormatCellValue(Row.GetCell(8)).Trim()),
+                            H = Int32.Parse(formatter.FormatCellValue(Row.GetCell(9)).Trim()),
+                            VV = GetValueOrNull<int>(formatter.FormatCellValue(Row.GetCell(10)).Trim()),
+                            WeatherPhenomena = formatter.FormatCellValue(Row.GetCell(11)).Trim()
                         };
 
                         weatherConditionsList.Add(weatherConditionsFromFile);
@@ -90,5 +134,14 @@ namespace WeatherArchive.Services.FileConverter
                 throw new Exception(ex.Message);
             }
         }
+
+        private T? GetValueOrNull<T>(string valueAsString) where T : struct
+        {
+            if (string.IsNullOrEmpty(valueAsString))
+                return null;
+            return (T)Convert.ChangeType(valueAsString, typeof(T));
+        }
+
+
     }
 }
